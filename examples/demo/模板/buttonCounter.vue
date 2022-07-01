4f4e21ea8792b5d749284ba8993bebc650e3cc8d
shencailiang <shen_cailiang@venusgroup.com.cn>
2022-07-01 14:39:32 +0800

    feat: vue例子提交

---
+++ examples/demo/模板/buttonCounter.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <button v-on:click="count++">
+    你已点击 <span class="counter">{{ count }}</span> 次数.
+  </button>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+<style scoped>
+  #app {
+    position: fixed;
+    top: 200px;
+    left: 400px;
+    right: 100px;
+    bottom: 100px;
+  }
+  .counter {
+    font-size: 30px;
+    color: red;
+  }
+</style>
